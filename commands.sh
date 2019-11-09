845a1caf7a768b62de2fa0582cbf857979ce8a79
Billy Andrews <scripture187@gmail.com>
2019-11-09 13:33:23 -0500

    Initial Commit

--- commands.sh
+++ commands.sh
@@ -1,2 +1,16 @@
-curl -X POST -H 'Content-type: application/json' --data @body.json https://hooks.slack.com/services/*********/*********/*********************
-curl -X POST -H 'Content-type: application/json' --data @error.json https://hooks.slack.com/services/*********/*********/*********************
+# Jenkins:
+#  Send a success message
+curl -X POST -H 'Content-type: application/json' --data @jenkinsSuccess.json https://hooks.slack.com/services/*********/*********/*********************
+#  Send a failure message
+curl -X POST -H 'Content-type: application/json' --data @jenkinsError.json https://hooks.slack.com/services/*********/*********/*********************
+
+
+# OSTicket:
+#  New Help Desk Ticket
+curl -X POST -H 'Content-type: application/json' --data @newOSTicket.json https://hooks.slack.com/services/*********/*********/*********************
+
+# Backups:
+#  Backup Completed:
+curl -X POST -H 'Content-type: application/json' --data @backupSuccess.json https://hooks.slack.com/services/*********/*********/*********************
+#  Backup Failure:
+curl -X POST -H 'Content-type: application/json' --data @backupFailure.json https://hooks.slack.com/services/*********/*********/*********************
